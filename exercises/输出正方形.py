eca3d9b596b07516a5e6cfb34c4b8d0122dcd5c5
kong bai <q2056423011@gmail.com>
2021-12-15 17:06:43 +0800

    python study

---
+++ exercises/输出正方形.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8 -*-
+# @author  :cddysq
+# @date    :2021/12/15 10:25
+# @version :v1.0.0
+
+i = 0
+while i < 5:
+    j = 0
+    while j < 5:
+        print("*  ", end='')
+        j += 1
+    print("")
+    i += 1
