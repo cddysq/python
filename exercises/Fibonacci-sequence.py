c847b444e5293ed53b16efa03b3f4f04ebd99008
kong bai <q2056423011@gmail.com>
2021-12-23 18:02:15 +0800

    py study

--- exercises/Fibonacci-sequence.py
+++ exercises/Fibonacci-sequence.py
@@ -6,7 +6,7 @@
 # 斐波那契数列：F(0)=0，F(1)=1, F(n)=F(n - 1)+F(n - 2)（n ≥ 2，n ∈ N*）
 
 def formula(n):
-    """ 返回斐波那契数列末一位f(n)的值 """
+    """ 返回斐波那契数列某一位f(n)的值 """
     if n == 0 or n == 1:
         return 1
     data = {0: 1, 1: 1}
@@ -15,8 +15,16 @@ def formula(n):
     return data[n]
 
 
+def recursion(n):
+    """ 递归方式返回斐波那契数列某一位f(n)的值 """
+    if n == 0 or n == 1:
+        return 1
+    return recursion(n - 1) + recursion(n - 2)
+
+
 n = input("请输入待计算的斐波那契数列n值:")
 while n.strip() == '':
     n = input("请输入待计算的斐波那契数列n值:")
 n = int(n)
 print(f"斐波那契数列：f({n})={formula(n)}")
+print(f"斐波那契数列：f({n})={recursion(n)}")
---
+++ exercises/File-operation.py
@@ -0,0 +1,64 @@
+# -*-coding:utf-8 -*-
+# @author  :cddysq
+# @date    :2021/12/23 16:04
+# @version :v1.0.0
+
+# 文件操作
+import os
+
+# 引入控制台颜色修改方法
+from library.consolecolor import *
+
+
+def create_a_file():
+    file_name = input("请输入待创建文件名,默认(like.txt):")
+    if file_name.strip() == '':
+        file_name = "like.txt"
+    content = input("请输入文件内容:")
+    if content.strip() == '':
+        content = "I love three things in this world. Sun, moon and you. Sun for morning, moon for night , and you forever ."
+    file_url = get_file_url(file_name)
+    file = open(file_url, "w+", encoding='utf-8')
+    file.write(content)
+    file.close()
+    print(FontColor.set_color(f"创建 {file_name} Success! 文件内容为：{content}", Colors.green))
+
+
+def get_file_url(file_name):
+    """ 返回文件完整路径 """
+    # 获取父路径
+    pardir = os.path.pardir
+    # 获取系统分割符
+    sep = os.sep
+    # 组装文件路径
+    file_url = f"{pardir}{sep}resources{sep}{file_name}"
+    return file_url
+
+
+def delete_the_file():
+    file_name = input("请输入需要删除的文件名:")
+    if file_name.strip() == '':
+        return
+    file_url = get_file_url(file_name)
+    if not os.path.exists(file_url):
+        print(FontColor.set_color(f"文件 {file_name} 不存在", Colors.Pink))
+        return
+    os.remove(file_url)
+    print(FontColor.set_color(f"文件 {file_name} 删除成功!", Colors.green))
+
+
+while True:
+    print(f"{'==' * 3}欢迎使用文件操作系统v1.0{'==' * 3}")
+    print("1.创建文件")
+    print("2.删除文件")
+    print("0.退出系统")
+    instruct = input("请输入对应编号进行操作:")
+    if instruct == '1':
+        create_a_file()
+    elif instruct == '2':
+        delete_the_file()
+    elif instruct == '0':
+        print("bye~")
+        break
+    else:
+        print(FontColor.set_color("指令错误,请重试!", Colors.red))
